c9debaedc2596cb8ee0a0351dab28ef55eccb35d
Max Lefebvre <max.lefebvre@garvin-allen.com>
2024-07-01 10:19:10 -0300

    fixed example app

---
+++ examples/vue3/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { MdiFileDocument, MdiCloudUploadOutline } from '@maxle5/mdi-vue';
+</script>
+
+<template>
+  <mdi-cloud-upload-outline style="color: red" :size="64" />
+  <mdi-file-document style="color: blue" :size="64" />
+</template>
--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,5 @@
 </template>
 
 <script setup lang="ts">
-import Abacus from "./components/Abacus.vue";
+import Abacus from "./components/MdiAbacus.vue";
 </script>
