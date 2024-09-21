4724fff9b71c08dab541d19b6f94f4152c0f3650
Max Lefebvre <max.lefebvre@garvin-allen.com>
2024-09-21 10:11:41 -0300

    gh action changes

---
+++ src/mdi-icon.d.ts
@@ -0,0 +1,29 @@
+export type MdiIcon = DefineComponent<
+  {
+    size: {
+      type: NumberConstructor;
+      default: number;
+    };
+  },
+  unknown,
+  unknown,
+  {},
+  {},
+  import("vue").ComponentOptionsMixin,
+  import("vue").ComponentOptionsMixin,
+  {},
+  string,
+  import("vue").PublicProps,
+  Readonly<
+    import("vue").ExtractPropTypes<{
+      size: {
+        type: NumberConstructor;
+        default: number;
+      };
+    }>
+  >,
+  {
+    size: number;
+  },
+  {}
+>;
